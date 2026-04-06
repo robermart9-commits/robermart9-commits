b8ede04b8cae93fcc0a002e7c21ae5fb3e7f086d
robermart9-commits <robermart9@gmail.com>
2026-04-06 16:41:37 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import {
 } from "lucide-react";
 import { motion, AnimatePresence } from "motion/react";
 import { useState, useEffect } from "react";
-
+import comidaImg from "./assets/comida.jpeg";
 
 // --- Components ---
 
