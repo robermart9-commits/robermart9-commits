3a6626f83f34a1c7c1ffcefae7ae33b1a4c8f23e
robermart9-commits <robermart9@gmail.com>
2026-04-06 16:51:59 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ const Hero = () => {
         >
           <div className="relative z-10 rounded-[40px] overflow-hidden shadow-2xl aspect-[4/5]">
             <img 
-              src="/imgcomida.jpeg"
+              src={comidaImg}
               alt="Healthy Food" 
               className="w-full h-full object-cover"
               referrerPolicy="no-referrer"
